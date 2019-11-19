d5e1c26ce0be36d30aa4fc9787dbbc11d6abd71d
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-11-19 16:41:26 -0600

    :alembic: Using cv2.HoughCircles
    Seems promising, but needs a lot of tunning.

--- Pregunta 6.py
+++ Pregunta 6.py
@@ -11,7 +11,7 @@
 # 
 # c. (1 punto) Continuando con la imagen anterior. Cuente y etiquete cuantos objetos de la segmentación pueden considerarse 2 células agrupadas, y cuantos y cuales más de 2 células.
 
-# In[1]:
+# In[6]:
 
 
 from functools import partial, reduce
@@ -20,7 +20,42 @@ import matplotlib.pyplot as plt
 import matplotlib.image as img
 
 import numpy as np
-import cv2 as cv
+import cv2 
+
+from mfilt_funcs import *
+from utils import *
+
+
+# In[11]:
+
+
+# Read image as gray-scale
+img  = cv2.imread('imagenes/Ex3Preg6(a).tif', cv2.IMREAD_GRAYSCALE)
+img2 = img.copy()
+# Blur the image to reduce noise
+img_blur = cv2.medianBlur(img, 5)
+# Apply hough transform on the image
+circles = cv2.HoughCircles(img_blur, cv2.HOUGH_GRADIENT, 1, img.shape[0]/64, param1=200, param2=10, minRadius=5, maxRadius=7)
+# Draw detected circles
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        # Draw outer circle
+        cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # Draw inner circle
+        #cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+
+# In[12]:
+
+
+side_by_side(img, img2)
+
+
+# In[10]:
+
+
+help(cv2.HoughCircles)
 
 
 # In[ ]:
