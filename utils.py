3c2d949f74946b9083918218d6a87ec30cceb672
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-11-20 00:06:58 -0600

    :children_crossing: utils.side_by_side() now supports color.
    If image is RGB, it will be ploted that way.

--- utils.py
+++ utils.py
@@ -101,12 +101,18 @@ def side_by_side(
     fig = plt.figure(figsize = _figsize)
     
     fig.add_subplot(1, 2, 1)
-    plt.imshow(image1, cmap = 'gray')
+    if len(image1.shape) == 3:
+        plt.imshow(image1)
+    else:
+        plt.imshow(image1, cmap = 'gray')
     if title1:
         plt.title(title1, size = 18)
     
     fig.add_subplot(1, 2, 2)
-    plt.imshow(image2, cmap = 'gray')
+    if len(image2.shape) == 3:
+        plt.imshow(image2)
+    else:
+        plt.imshow(image2, cmap = 'gray')
     if title2:
         plt.title(title2, size = 18)
 ##
