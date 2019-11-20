ae541af8a6b141af234e00b768149283abd2b7c8
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-11-20 17:24:31 -0600

    :bulb: Added some explanations.
    .

--- Pregunta 6.py
+++ Pregunta 6.py
@@ -11,7 +11,7 @@
 # 
 # c. (1 punto) Continuando con la imagen anterior. Cuente y etiquete cuantos objetos de la segmentación pueden considerarse 2 células agrupadas, y cuantos y cuales más de 2 células.
 
-# In[150]:
+# In[1]:
 
 
 # Functional programing tools : 
@@ -43,7 +43,7 @@ from sklearn.cluster import KMeans
 import importlib
 
 
-# In[130]:
+# In[2]:
 
 
 # Custom :
@@ -63,42 +63,42 @@ import utils
 #plt.style.available
 
 
-# In[39]:
+# In[4]:
 
 
 plt.style.use('seaborn-deep')
 plt.rcParams['figure.figsize'] = (10, 5)
 
 
-# In[77]:
+# In[5]:
 
 
 img     = cv.imread('imagenes/Ex3Preg6(a).tif', cv.IMREAD_GRAYSCALE)
 color   = cv.cvtColor(img, cv.COLOR_GRAY2RGB) # Color copy, to draw colored circles
 
 
-# a. (0.5 puntos) Usando una técnica de umbralización global, segmente la imagen y muestre el resultado de la segmetnación.
+# ### a. (0.5 puntos) Usando una técnica de umbralización global, segmente la imagen y muestre el resultado de la segmetnación.
 
-# In[94]:
+# In[6]:
 
 
 intensities = pd.core.frame.DataFrame(dict(intensity=img.flatten()))
 
 
-# In[95]:
+# In[7]:
 
 
 intensities.hist()
 
 
-# In[96]:
+# In[8]:
 
 
 kmeans = KMeans(n_clusters=2, random_state=0, verbose=False).fit(intensities)
 K = kmeans.cluster_centers_.mean()
 
 
-# In[97]:
+# In[9]:
 
 
 intensities.hist()
@@ -107,13 +107,13 @@ list(map(lambda x: plt.axvline(x, color='g'), kmeans.cluster_centers_))
 _ = plt.title(f"Means = {kmeans.cluster_centers_.tolist()}, K = {K}", size=16)
 
 
-# In[99]:
+# In[10]:
 
 
 thresh1 = cv.threshold(img, K, 255, cv.THRESH_BINARY)[1]
 
 
-# In[100]:
+# In[11]:
 
 
 utils.side_by_side(img, thresh1)
@@ -121,13 +121,13 @@ utils.side_by_side(img, thresh1)
 
 # Como podemos ver, una técinca de umbralización estándar como k-medias móviles, con dos medias, da resultados muy pobres.
 
-# In[101]:
+# In[12]:
 
 
 otsu1 = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
 
 
-# In[102]:
+# In[13]:
 
 
 utils.side_by_side(img, otsu1)
@@ -135,14 +135,14 @@ utils.side_by_side(img, otsu1)
 
 # El algoritmo de Otsu no logra mejorar mucho la segmentación (esto era de esperarse dado que el histograma original era claramente bimodal). 
 
-# In[109]:
+# In[14]:
 
 
 gblur = cv.GaussianBlur(img,(3,3),0)
 otsu2 = cv.threshold(gblur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
 
 
-# In[110]:
+# In[15]:
 
 
 utils.side_by_side(img, otsu2)
@@ -150,36 +150,43 @@ utils.side_by_side(img, otsu2)
 
 # El algoritmo de Otsu no logra mejorar mucho la segmentación aún en combinación con un suavizado Gaussiano.
 
-# In[136]:
+# ### b. (0.5 puntos) A la imagenoriginal se le aplicó una umbralización con valores locales yal resultado se le realizó una apertura morbológica obteniendo la imagen Ex3Preg6(b).tif. Usando esta imagen,cuente y etiquete cuantos objetos de la segmentación pueden considerarse células independientes. 
+
+# In[20]:
 
 
-imgb = cv.imread('imagenes/Ex3Preg6(b).tif', cv.IMREAD_GRAYSCALE)
+imgb  = cv.imread('imagenes/Ex3Preg6(b).tif', cv.IMREAD_GRAYSCALE)
 imgbc = cv.cvtColor(imgb, cv.COLOR_BAYER_GB2RGB)
 utils.side_by_side(imgb, imgbc)
 
 
-# In[142]:
+# ### Primera aproximación :
+# El uso de la transformada de Hough para círculos, no para líneas. Al ver la imagen, uno podría pensar que un círculo es una buena aproximación de la forma de una célula, por lo tanto los cículos encontrados por una transformada de Hough serían las células que buscamos identificar, caracterizar y contabilizar
+
+# In[21]:
 
 
 circles = cv.HoughCircles(imgb, cv.HOUGH_GRADIENT, 1, img.shape[0]/64, param1=200, param2=10, minRadius=5, maxRadius=15)
 
 
-# In[143]:
+# In[24]:
 
 
-# Draw detected circles
 if circles is not None:
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
-        # Draw outer circle
         cv.circle(imgbc, (i[0], i[1]), i[2], (155, 0, 0), 2)
 
 
-# In[144]:
+# In[25]:
 
 
 utils.side_by_side(imgb, imgbc)
 
+
+# Aquí podemos ver que aunque la imagen principal carece de falsos positivos (es decir todos los círculos dibujados dentro de la región útil de la imagen contienen una célula) el **número de falsos negativos es altísimo** : sólo una pequeña parte de las células observadas fueron identificadas por ```cv.HoughCircles()```. 
+# 
+# Esto nos indica que tal vez las células no se asemejan tanto a un círculo. Por esta razón, no se explorará más a fondo esta vía de acción. Cabe mencionar que la transformada encuentra círculos en el texto de encabezado : **Imagen con apertura**. Por esta razón, en delante se trabajará con otra imagen recortada a mano para excluir este texto que podría causar problemas en la segmentación más adelante.
 
 # # Extra
 
