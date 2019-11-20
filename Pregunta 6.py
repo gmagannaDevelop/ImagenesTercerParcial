7cb4ea2840856395902f346a480693b56cf38801
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-11-20 00:43:11 -0600

    :boom: Included skimage
    .

--- Pregunta 6.py
+++ Pregunta 6.py
@@ -11,7 +11,7 @@
 # 
 # c. (1 punto) Continuando con la imagen anterior. Cuente y etiquete cuantos objetos de la segmentación pueden considerarse 2 células agrupadas, y cuantos y cuales más de 2 células.
 
-# In[1]:
+# In[150]:
 
 
 # Functional programing tools : 
@@ -20,6 +20,7 @@ from functools import partial, reduce
 # Visualisation : 
 import matplotlib.pyplot as plt
 import matplotlib.image as pim
+import matplotlib.patches as mpatches
 import seaborn as sns
 
 # Data tools :
@@ -28,6 +29,12 @@ import pandas as pd
 
 # Image processing : 
 import cv2 as cv
+from skimage import data
+from skimage.filters import threshold_otsu
+from skimage.segmentation import clear_border
+from skimage.measure import label, regionprops
+from skimage.morphology import closing, square
+from skimage.color import label2rgb
 
 # Machine Learning :
 from sklearn.cluster import KMeans
@@ -36,7 +43,7 @@ from sklearn.cluster import KMeans
 import importlib
 
 
-# In[2]:
+# In[130]:
 
 
 # Custom :
@@ -72,26 +79,26 @@ color   = cv.cvtColor(img, cv.COLOR_GRAY2RGB) # Color copy, to draw colored circ
 
 # a. (0.5 puntos) Usando una técnica de umbralización global, segmente la imagen y muestre el resultado de la segmetnación.
 
-# In[41]:
+# In[94]:
 
 
-intensities = pd.core.frame.DataFrame(dict(intensity=gthresh.flatten()))
+intensities = pd.core.frame.DataFrame(dict(intensity=img.flatten()))
 
 
-# In[42]:
+# In[95]:
 
 
 intensities.hist()
 
 
-# In[43]:
+# In[96]:
 
 
 kmeans = KMeans(n_clusters=2, random_state=0, verbose=False).fit(intensities)
 K = kmeans.cluster_centers_.mean()
 
 
-# In[74]:
+# In[97]:
 
 
 intensities.hist()
@@ -100,27 +107,13 @@ list(map(lambda x: plt.axvline(x, color='g'), kmeans.cluster_centers_))
 _ = plt.title(f"Means = {kmeans.cluster_centers_.tolist()}, K = {K}", size=16)
 
 
-# In[89]:
+# In[99]:
 
 
-gthresh = img.copy()
-gthresh[ gthresh >= K ] = 255
-gthresh[ gthresh < K ] = 0 
+thresh1 = cv.threshold(img, K, 255, cv.THRESH_BINARY)[1]
 
 
-# In[90]:
-
-
-utils.side_by_side(img, gthresh)
-
-
-# In[86]:
-
-
-ret, thresh1 = cv.threshold(img, K, 255, cv.THRESH_BINARY)
-
-
-# In[87]:
+# In[100]:
 
 
 utils.side_by_side(img, thresh1)
@@ -128,21 +121,50 @@ utils.side_by_side(img, thresh1)
 
 # Como podemos ver, una técinca de umbralización estándar como k-medias móviles, con dos medias, da resultados muy pobres.
 
-# In[24]:
+# In[101]:
 
 
-# Blur the image to reduce noise
-img_blur = cv.medianBlur(img, 5)
+otsu1 = cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
 
 
-# In[34]:
+# In[102]:
 
 
-# Apply hough transform on the image
-circles = cv.HoughCircles(img_blur, cv.HOUGH_GRADIENT, 1, img.shape[0]/64, param1=200, param2=10, minRadius=5, maxRadius=7)
+utils.side_by_side(img, otsu1)
 
 
-# In[35]:
+# El algoritmo de Otsu no logra mejorar mucho la segmentación (esto era de esperarse dado que el histograma original era claramente bimodal). 
+
+# In[109]:
+
+
+gblur = cv.GaussianBlur(img,(3,3),0)
+otsu2 = cv.threshold(gblur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)[1]
+
+
+# In[110]:
+
+
+utils.side_by_side(img, otsu2)
+
+
+# El algoritmo de Otsu no logra mejorar mucho la segmentación aún en combinación con un suavizado Gaussiano.
+
+# In[136]:
+
+
+imgb = cv.imread('imagenes/Ex3Preg6(b).tif', cv.IMREAD_GRAYSCALE)
+imgbc = cv.cvtColor(imgb, cv.COLOR_BAYER_GB2RGB)
+utils.side_by_side(imgb, imgbc)
+
+
+# In[142]:
+
+
+circles = cv.HoughCircles(imgb, cv.HOUGH_GRADIENT, 1, img.shape[0]/64, param1=200, param2=10, minRadius=5, maxRadius=15)
+
+
+# In[143]:
 
 
 # Draw detected circles
@@ -150,33 +172,132 @@ if circles is not None:
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
         # Draw outer circle
-        cv.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        cv.circle(imgbc, (i[0], i[1]), i[2], (155, 0, 0), 2)
+
+
+# In[144]:
+
+
+utils.side_by_side(imgb, imgbc)
+
+
+# # Extra
+
+# In[115]:
+
+
+# Blur the image to reduce noise
+img_blur = cv.medianBlur(img, 5)
+
+
+# In[116]:
+
+
+# Apply hough transform on the image
+circles = cv.HoughCircles(img_blur, cv.HOUGH_GRADIENT, 1, img.shape[0]/64, param1=200, param2=10, minRadius=5, maxRadius=7)
+
+
+# In[117]:
+
+
+# Draw detected circles
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        # Draw outer circle
+        cv.circle(img, (i[0], i[1]), i[2], (0, 0, 255), 2)
         # Draw inner circle
         #cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
 
 
-# In[36]:
+# In[121]:
 
 
-side_by_side(img, color)
+#plt.imshow(img)
+img.shape
 
 
-# In[28]:
+# In[119]:
 
 
-help(cv2.HoughCircles)
+utils.side_by_side(img, color)
 
 
-# In[26]:
+# In[162]:
 
 
-help(cv2.cvtColor)
+label_image, n_objs = label(imgb, return_num=True)
+plt.imshow(label_image)
+print(n_objs)
 
 
-# In[ ]:
+# # Scikit-Image example :
+
+# In[151]:
 
 
+image = data.coins()[50:-50, 50:-50]
 
+# apply threshold
+thresh = threshold_otsu(image)
+bw = closing(image > thresh, square(3))
+plt.imshow(bw)
+
+
+# In[152]:
+
+
+# remove artifacts connected to image border
+cleared = clear_border(bw)
+plt.imshow(cleared)
+
+
+# In[154]:
+
+
+# label image regions
+label_image = label(cleared)
+plt.imshow(label_image)
+
+
+# In[155]:
+
+
+image_label_overlay = label2rgb(label_image, image=image)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.imshow(image_label_overlay)
+
+for region in regionprops(label_image):
+    # take regions with large enough areas
+    if region.area >= 100:
+        # draw rectangle around segmented coins
+        minr, minc, maxr, maxc = region.bbox
+        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                  fill=False, edgecolor='red', linewidth=2)
+        ax.add_patch(rect)
+
+ax.set_axis_off()
+plt.tight_layout()
+plt.show()
+
+
+# In[147]:
+
+
+#help(cv2.HoughCircles)
+
+
+# In[148]:
+
+
+#help(cv2.cvtColor)
+
+
+# In[158]:
+
+
+help(label)
 
 
 # In[ ]:
